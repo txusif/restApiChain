c93d4489f32f8629aceea687687000e054ae0552
Txusif <txusif@gmail.com>
2023-08-02 21:09:33 +0530

    restApi

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/:docId", (req, res) => {
+  const docId = req.params.docId;
+  const docList = ["a", "b", "c"];
+  if (docList.includes(docId)) {
+    res.send({ res: "100" });
+  } else {
+    res.send({ res: "0" });
+  }
+});
+
+app.get("/tldr", (req, res) => {
+  res.send("Hello Express App");
+});
+
+app.listen(port, () => {
+  console.log("Server started!");
+});
